0326ac3ea7766c98340c7944ef9a986530a93f72
Gary Wilk <garywilk2015@u.northwestern.edu>
2018-11-20 12:59:56 -0600

    updated data loading path and renamed objects

--- scripts/1_createSNPdb.R
+++ scripts/1_createSNPdb.R
@@ -1,7 +1,7 @@
 # get annotated info for affy snp ids & create R db object
 
 # change to project repo if necessary
-load("scratch/data/SNPids.RData")
+load("data/SNPids.RData")
 library(pd.genomewidesnp.6)
 
 #==============================================================================
@@ -10,23 +10,23 @@ library(pd.genomewidesnp.6)
 
 get(objects("package:pd.genomewidesnp.6"))
 con6 = get(objects("package:pd.genomewidesnp.6"))@getdb()
-SNPdb.df <- dbGetQuery(con6, 
+SNPdb <- dbGetQuery(con6, 
 	paste0("select * from featureSet where man_fsetid in ('", 
                         paste0(SNPids, collapse = "','"), "')"))
 
 # parse string to get ensembl id and gene symbol: X[1] ensembl, X[5] gene name
-geneIDs <- sapply(strsplit(SNPdb.df$gene_assoc, " // "), function(X) {
+geneIDs <- sapply(strsplit(SNPdb$gene_assoc, " // "), function(X) {
 	c(X[1], X[5])
-	})
-SNPdb.df$gene <- geneIDs[2, ]
-SNPdb.df$ensembl <- geneIDs[1, ]
+})
+SNPdb$gene <- geneIDs[2, ]
+SNPdb$ensembl <- geneIDs[1, ]
 
 #==============================================================================
 # save database objects
 #==============================================================================
 
 # remove extraneous columns and save
-SNPdb.df <- subset(SNPdb.df, select = -c(cnv, gene_assoc))
+SNPdb.df <- subset(SNPdb, select = -c(cnv, gene_assoc))
 rownames(SNPdb.df) <- SNPdb.df$man_fsetid
 save(SNPdb.df, file = "data/SNPdb.df.RData")
 
