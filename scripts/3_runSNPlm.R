109014d1a3505ab35fabab0799b31a77f638aff0
Gary Wilk <garywilk2015@u.northwestern.edu>
2018-11-20 15:49:13 -0600

    finds unique trio combos in breast cancer

--- scripts/3_runSNPlm.R
+++ scripts/3_runSNPlm.R
@@ -1,9 +1,6 @@
-# runs linear model for all unique gene~miRNA:SNP unique combinations 
-# found on dysregulated pathways by miRNAs in tumors
-#
-# NOTE! because of large size, uses multiple cores to run code, can modify
-#       to run on a single core
-#       
+# finds unique gene:mir:SNP combinations on dysregulated KEGG pathways
+# this script references breast cancer data
+#      
 # script assumes miRNA-pathway dysregulation stats are already available 
 #     1) "BRCAstats1e5.RData" dataset contains pathway dysregulation by 
 #        miRNAs in tumors
@@ -21,41 +18,40 @@ library(dplyr)
 # clean objects & get pairs
 #==============================================================================
 
-# filters genes/SNPs to only those assayed in data
+# filters genes & SNPs to only those assayed in data
 pathGenes <- lapply(getPathwayGenes(), function(P) {
 	intersect(P, rownames(BRCAdata$gene))
 })
-SNPbyGene <- SNPbyGene[unique(unlist(pathGenes))]
-SNPbyGene <- lapply(SNPbyGene, function(S) intersect(S, names(BRCAdata$mut)))
-SNPbyGene <- SNPbyGene[sapply(SNPbyGene, length) > 0]
-pathGenes <- lapply(pathGenes, function(P) intersect(P, names(SNPbyGene)))
+SNPbyGene.uni <- SNPbyGene[unique(unlist(pathGenes))]
+SNPbyGene.uni.int <- lapply(SNPbyGene.uni, function(S) {
+	intersect(S, names(BRCAdata$mut))
+})
+SNPbyGene.filt <- SNPbyGene.uni.int[sapply(SNPbyGene.uni.int, length) > 0]
+pathGenes.filt <- lapply(pathGenes, function(P) {
+	intersect(P, names(SNPbyGene.filt))
+})
 
 # get most significant miR-path pairs
-BRCAstats1e5 <- subset(BRCAstats1e5, pval < 0.01)
+BRCAstats1e5.sig <- subset(BRCAstats1e5, pval < 0.01)
 
 #==============================================================================
-# precompute all unique gene-mir-snp combinations
+# precompute all unique gene:mir:snp combinations
 #==============================================================================
 
 # df of all combinations to run
 BRCAanova <- mapply(function(M, P) {
-  makeDF(miRNA = M, pathway = P, pathwayGenes = pathGenes, db = SNPbyGene)
-}, BRCAstats1e5$miRNA, BRCAstats1e5$pathway, SIMPLIFY = FALSE)
+  makeDF(miRNA = M, pathway = P, 
+  	pathwayGenes = pathGenes.filt, db = SNPbyGene.filt)
+}, BRCAstats1e5.sig$miRNA, BRCAstats1e5.sig$pathway, SIMPLIFY = FALSE)
 BRCAanova <- rbind_all(BRCAanova)
 BRCAanova <- BRCAanova[!duplicated(BRCAanova[, c("miRNA", "gene", "snp")]), ]
 
-# remove objects
-rm(BRCAstats1e5, SNPbyGene, pathGenes)
+# garbage collect
+rm(BRCAstats1e5, SNPbyGene, SNPbyGene.uni, SNPbyGene.uni.int, pathGenes)
 
 #==============================================================================
-# run on significant pairs
+# save combos
 #==============================================================================
-
-# run on all combinations
-BRCAanova$pval <- mcmapply(function(G, M, S) {
-  runANOVA(gene = G, mir = M, snp = S, data = BRCAdata)
-}, BRCAanova$gene, BRCAanova$miRNA, 
-BRCAanova$snp, mc.cores = 8, USE.NAMES = FALSE)
 
 # save
 save(BRCAanova, file = "data/BRCAanova.RData")
