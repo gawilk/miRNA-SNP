b36058db6a535daeccd4963c4081f8b6fb56a1f4
Gary Wilk <garywilk2015@u.northwestern.edu>
2018-12-04 12:46:35 -0600

    plot regQTL interactions with PCA substructure correction

--- scripts/7_plotTrio.R
+++ scripts/7_plotTrio.R
@@ -1,19 +1,56 @@
-# plots a gene~miRNA:SNP interaction trio
+# plots regQTL interaction trios w/ PCA substructure correction
+# regQTL interactions are plotted for breast tumors
 # 
 # change to project repo if necessary
-load("data/BRCAanovaTUMOR.RData")
+load("data/BRCAdata.RData")
+load("data/BRCAanovaTUMOR_PCA.RData")
+load("data/BRCAevv.RData")
 load("data/SNPdb.df.RData")
-source("source/srcFun.R")
+source("source/srcFns.R")
 library(ggplot2)
+library(gridExtra)
 
 #==============================================================================
-# plot trio
+# reorder regQTLs by significance
 #==============================================================================
 
-# pick any trio! here's trio from 5th row
-combo <- BRCAanovaTUMOR[5, ]
-g1 <- plotSNPTUMOR(miRNA = combo$miRNA, gene = combo$gene, 
-             mut = combo$snp, data = BRCAdata, filter = TRUE, 
-             method = "cook.value", threshold = 1, 
-             maintitle = "", db = SNPdb.df)
-g1
+BRCAanovaTUMOR_PCA.ord <- BRCAanovaTUMOR_PCA[order(BRCAanovaTUMOR_PCA$pcookONE), ]
+
+#==============================================================================
+# some significant regQTLs
+#==============================================================================
+
+g1 <- plotSNPTUMORPCA(gene = BRCAanovaTUMOR_PCA.ord$gene[2], 
+                      PCs = BRCAevv$vectors, 
+                      mir = BRCAanovaTUMOR_PCA.ord$miRNA[2],
+                      snp = BRCAanovaTUMOR_PCA.ord$snp[2], 
+                      data = BRCAdata, 
+                      method = "cook.value",
+                      maintitle = "", 
+                      db = SNPdb.df)
+
+g2 <- plotSNPTUMORPCA(gene = BRCAanovaTUMOR_PCA.ord$gene[9], 
+                      PCs = BRCAevv$vectors, 
+                      mir = BRCAanovaTUMOR_PCA.ord$miRNA[9],
+                      snp = BRCAanovaTUMOR_PCA.ord$snp[9], 
+                      data = BRCAdata, 
+                      method = "cook.value",
+                      maintitle = "", 
+                      db = SNPdb.df)
+
+g3 <- plotSNPTUMORPCA(gene = BRCAanovaTUMOR_PCA.ord$gene[10], 
+                      PCs = BRCAevv$vectors, 
+                      mir = BRCAanovaTUMOR_PCA.ord$miRNA[10],
+                      snp = BRCAanovaTUMOR_PCA.ord$snp[10], 
+                      data = BRCAdata, 
+                      method = "cook.value",
+                      maintitle = "", 
+                      db = SNPdb.df)
+
+BRplots <- list(g1, g2, g3)
+
+#==============================================================================
+# plot regQTLs
+#==============================================================================
+
+do.call(grid.arrange, c(BRplots, ncol = 3))
